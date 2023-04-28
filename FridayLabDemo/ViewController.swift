262002443d97e7c97221d35c0d9a11a74f2fc9a4
williamhartono <106573860+williamhartono@users.noreply.github.com>
2023-04-28 10:06:23 +1000

    Update ViewController.swift
    Merging

--- FridayLabDemo/ViewController.swift
+++ FridayLabDemo/ViewController.swift
@@ -13,7 +13,7 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         // More changes
-        print("Test for changes")
+        print("Test for another conflict")
     }
 
 
